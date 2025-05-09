26eda27f4004cea51bb2cdc4d9994199ce14cc3b
Caio Diniz <caiodiniz200204@gmail.com>
2025-05-09 13:20:18 -0300

    Update: Pacotes Externos

--- Secao 2 -Fundamentos da Linguagem/1 - Pacotes/go.mod
+++ Secao 2 -Fundamentos da Linguagem/1 - Pacotes/go.mod
@@ -1,3 +1,10 @@
 module modulos
 
 go 1.24.3
+
+// importando pacotes externos 
+//go.mod → diz quais pacotes você usa e em qual versão.
+//go.sum → garante que essas versões não mudaram ou foram corrompidas.
+
+require github.com/badoux/checkmail v1.2.4 // indirect
+// caso queira remover os pacotes externsos, usar o comando go mod tydi
--- Secao 2 -Fundamentos da Linguagem/1 - Pacotes/main.go
+++ Secao 2 -Fundamentos da Linguagem/1 - Pacotes/main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 	auxiliar "modulos/Auxiliar"
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo do arquivo main...")
 	auxiliar.Escrever() //na main eu so consigo referenciar funcoes de outros pacotes como o auxiliar se ele comecar com letra Maiuscula.
 	// mas no msm pacote eu consigo referenciar uma funcao comecando com letra minuscula, assim como esta no auxiliar.go
+
+	erro := checkmail.ValidateFormat("cursodeGO@gmail.com") // Pacote Externo de checkmail, usado a partir do go get github.com/badoux/checkmail@latest e adicionado automaticamente no go.mod e go.sum
+	fmt.Println(erro)
+	// caso retorne "nulo" (<nil>), significa que o email e valido
+	// caso o contráio irá retornar "invalid format"
 }
