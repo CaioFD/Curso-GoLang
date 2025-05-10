8d1206b48fe197f0444ba57268094dc2f334e7db
Caio Diniz <caiodiniz200204@gmail.com>
2025-05-10 10:12:02 -0300

    Update Secao 1

--- Secao 1 - Introducao/0 - Comecando/PrimeirosPassos.go
+++ Secao 1 - Introducao/0 - Comecando/PrimeirosPassos.go
@@ -1,5 +1,7 @@
 package main // todos os programas sao organizdos em pacotes
 
+// No GO nao precisa de "";"" pois o proprio compilador adiciona quando vai rodar o codigo
+
 import ( // Referenciamos os pacotes que vamos usar no programa
 		"fmt" // fornece funções para formatação e impressão de texto, incluindo a função fmt.Println()
 		//"string" 
